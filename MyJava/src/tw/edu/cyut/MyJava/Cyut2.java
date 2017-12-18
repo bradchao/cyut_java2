a29fd582077d1a0d387d4d88e8824a87ae9430e8
E517 <E517@E517-58>
2017-12-18 11:12:49 +0800

    2017121804

--- MyJava/src/tw/edu/cyut/MyJava/Cyut2.java
+++ MyJava/src/tw/edu/cyut/MyJava/Cyut2.java
@@ -1,5 +1,10 @@
 package tw.edu.cyut.MyJava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Cyut2 {
 
 	public static void main(String[] args) {
