1ef61ddc645b6148149113d9128021f1957ca43e
E517 <E517@E517-58>
2017-12-18 10:16:51 +0800

    2017121802

--- MyJava/src/tw/edu/cyut/MyJava/Cyut2.java
+++ MyJava/src/tw/edu/cyut/MyJava/Cyut2.java
@@ -7,6 +7,25 @@ public class Cyut2 {
 		int score = (int)(value * 49) +1;
 		System.out.println(score);
 		System.out.println(Math.PI);
+		
+		System.out.println(Bike.count);
+		Bike b1 = new Bike(2);
+		Bike b2 = new Bike(3);
+		Bike b3 = new Bike(4);
+		System.out.println(b1.speed);
+		System.out.println(b2.speed);
+		System.out.println(b3.speed);
+		System.out.println(Bike.count);
+		System.out.println(b2.count);
+		
 	}
 
+}
+class Bike {
+	double speed;
+	static int count;
+	Bike(double d){
+		speed = d;
+		count++;
+	}
 }
