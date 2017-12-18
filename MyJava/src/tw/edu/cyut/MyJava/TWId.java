d980fccf5450eade63467528e48f4a520fcb6888
E517 <E517@E517-58>
2017-12-18 11:16:40 +0800

    2017121805

--- MyJava/src/tw/edu/cyut/MyJava/TWId.java
+++ MyJava/src/tw/edu/cyut/MyJava/TWId.java
@@ -4,13 +4,13 @@ public class TWId {
 	private String id;
 	
 	public TWId() {
-		
+		this((int)(Math.random()*2)==1?true:false,(int)(Math.random()*26));
 	}
 	public TWId(boolean isMale) {
-		
+		this(isMale, (int)(Math.random()*26));
 	}
 	public TWId(int area) {
-		
+		this((int)(Math.random()*2)==1?true:false , area);
 	}
 	public TWId(boolean isMale, int area) {
 		
